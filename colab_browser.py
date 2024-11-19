71a622fd3b25d1ec0d3ec217833f7885729d3562
Iorimiya <iorimiya.vina@gmail.com>
2024-11-19 22:09:48 +0800

    add code

---
+++ colab_browser.py
@@ -0,0 +1,45 @@
+import time
+import logging
+from pathlib import Path
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from urllib3.exceptions import ReadTimeoutError
+
+
+class ColabBrowser(webdriver.Chrome):
+
+    def __init__(self, target_url: str = None) -> None:
+        # options to hide windows appear
+        self.__options = Options()
+        self.__options.add_argument("--headless")
+        self.__options.add_argument("--no-sandbox")
+        self.__options.add_argument("--disable-dev-shm-usage")
+        self.__options.add_argument("--disable-gpu")
+        self.__options.add_argument("--window-size=1600,900")
+        # options to change defaults download dir
+        download_path = Path(__file__).resolve(strict=True).parent.joinpath("data")
+        experimental_option = {"download.default_directory": str(download_path)}
+        self.__options.add_experimental_option(name="prefs", value=experimental_option)
+        logging.info(f"download path switch to {download_path}")
+
+        # create web driver and go to base_url
+        super().__init__(options=self.__options)
+
+        if target_url:
+            self.get(url=target_url)
+
+    def __enter__(self) -> any:
+        return super().__enter__()
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> any:
+        super().__exit__(exc_type, exc_val, exc_tb)
+
+    def get(self, url: str, waiting_time: int = 0.05) -> None:
+        try:
+            super().get(url)
+        except ReadTimeoutError:
+            logging.exception(f'open URL {url} failed.')
+            exit(1)
+        else:
+            logging.info(f"goto url: {self.current_url}")
+            time.sleep(waiting_time)
