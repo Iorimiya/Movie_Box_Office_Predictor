ce9e7ab1799d8e9d8982c136b539826c148d0db2
Iorimiya <iorimiya.vina@gmail.com>
2024-11-21 18:50:36 +0800

    coding

--- colab_browser.py
+++ colab_browser.py
@@ -9,18 +9,19 @@ from urllib3.exceptions import ReadTimeoutError
 class ColabBrowser(webdriver.Chrome):
 
     def __init__(self, target_url: str = None) -> None:
-        # options to hide windows appear
+        # options
         self.__options = Options()
         self.__options.add_argument("--headless")
         self.__options.add_argument("--no-sandbox")
         self.__options.add_argument("--disable-dev-shm-usage")
         self.__options.add_argument("--disable-gpu")
         self.__options.add_argument("--window-size=1600,900")
+
         # options to change defaults download dir
         download_path = Path(__file__).resolve(strict=True).parent.joinpath("data")
         experimental_option = {"download.default_directory": str(download_path)}
         self.__options.add_experimental_option(name="prefs", value=experimental_option)
-        logging.info(f"download path switch to {download_path}")
+        logging.info(msg="download path switch to {download_path}")
 
         # create web driver and go to base_url
         super().__init__(options=self.__options)
@@ -38,8 +39,8 @@ class ColabBrowser(webdriver.Chrome):
         try:
             super().get(url)
         except ReadTimeoutError:
-            logging.exception(f'open URL {url} failed.')
-            exit(1)
+            logging.error(msg=f'open URL "{url}" failed.')
         else:
             logging.info(f"goto url: {self.current_url}")
+        finally:
             time.sleep(waiting_time)
