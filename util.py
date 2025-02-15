33c2791ef6dbc76d0428bea041735cddd2c14ff9
Iorimiya <iorimiya.vina@gmail.com>
2025-02-15 18:36:09 +0800

    constant fix

--- util.py
+++ util.py
@@ -1,25 +1,18 @@
 import csv
-from enum import Enum
 from pathlib import Path
-from typing import TypedDict
+from typing import TypedDict,Final
 
-
-class Constant(Enum):
-    STATUS_BAR_FORMAT:str = '{desc}: {percentage:3.2f}%|{bar}{r_bar}'
-
-    class Headers(Enum):
-        INDEX_HEADER: list[str] = ['id', 'name']
-        INPUT_MOVIE_LIST_HEADER: str = 'movie_name'
-        BOX_OFFICE_DOWNLOAD_PROGRESS_HEADER: list[str] = ['id', 'movie_page_url', 'file_path']
-
-    class Paths(Enum):
-        PROJECT_FOLDER: Path = Path(__file__).parent
-        DATA_FOLDER: Path = PROJECT_FOLDER.value.joinpath('data')
-        BOX_OFFICE_FOLDER: Path = DATA_FOLDER.value.joinpath('web_scraping_data', 'box_office')
-        PUBLIC_REVIEW_FOLDER: Path = DATA_FOLDER.value.joinpath('web_scraping_data', 'public_review')
-        EXPERT_REVIEW_FOLDER: Path = DATA_FOLDER.value.joinpath('web_scraping_data', 'expert_review')
-        INDEX_PATH: Path = DATA_FOLDER.value.joinpath('web_scraping_data', 'index.csv')
-
+class Constants:
+    STATUS_BAR_FORMAT:Final[str] = '{desc}: {percentage:3.2f}%|{bar}{r_bar}'
+    INDEX_HEADER: Final[list[str]] = ['id', 'name']
+    INPUT_MOVIE_LIST_HEADER: Final[str] = 'movie_name'
+    BOX_OFFICE_DOWNLOAD_PROGRESS_HEADER: Final[list[str]] = ['id', 'movie_page_url', 'file_path']
+    PROJECT_FOLDER: Final[Path] = Path(__file__).parent
+    DATA_FOLDER: Final[Path] = PROJECT_FOLDER.joinpath('data')
+    BOX_OFFICE_FOLDER: Final[Path] = DATA_FOLDER.joinpath('web_scraping_data', 'box_office')
+    PUBLIC_REVIEW_FOLDER: Final[Path] = DATA_FOLDER.joinpath('web_scraping_data', 'public_review')
+    EXPERT_REVIEW_FOLDER: Final[Path] = DATA_FOLDER.joinpath('web_scraping_data', 'expert_review')
+    INDEX_PATH: Final[Path] = DATA_FOLDER.joinpath('index.csv')
 
 
 class CSVFileData(TypedDict):
@@ -43,7 +36,7 @@ def write_data_to_csv(path: Path, data: list[dict], header: list) -> None:
 def initialize_index_file(input_file: CSVFileData, index_file: CSVFileData | None = None) -> None:
     # get movie names from input csv
     if index_file is None:
-        index_file = CSVFileData(path=Constant.Paths.INDEX_PATH.value, header=Constant.Headers.INDEX_HEADER.value)
+        index_file = CSVFileData(path=Constants.INDEX_PATH, header=Constants.INDEX_HEADER)
     with open(file=input_file['path'], mode='r', encoding='utf-8') as file:
         movie_names: list[str] = [row[input_file['header']] for row in csv.DictReader(file)]
     # create index file
